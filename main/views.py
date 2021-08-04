3f715fe7a6b8e604040405504ebe43f21cb20fc4
waleed alqallaf <waleedalqalaf2@gmail.com>
2021-08-04 21:28:08 +0300

    added simple permissions

--- main/views.py
+++ main/views.py
@@ -2,13 +2,33 @@ from rest_framework import viewsets, mixins
 from .serializers import *
 from .models import *
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+class ActionPermissions:
+     def get_permissions(self):
+        try:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError: 
+            return [permission() for permission in self.permission_classes]
 
 # ViewSets define the view behavior.
-class ProjectView(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin):
+class ProjectView(ActionPermissions, viewsets.GenericViewSet,
+                mixins.ListModelMixin,
+                mixins.CreateModelMixin,
+                mixins.RetrieveModelMixin):
+    permission_classes = (IsAuthenticated,)
+    permission_classes_by_action = {'list': [AllowAny]}
+    
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
-class ProjectIssuesView(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin):
+class ProjectIssuesView(ActionPermissions, viewsets.GenericViewSet, 
+                        mixins.ListModelMixin, 
+                        mixins.RetrieveModelMixin, 
+                        mixins.CreateModelMixin):
+    permission_classes = (IsAuthenticated,)
+    permission_classes_by_action = {'list': [AllowAny]}
+
     queryset = Issue.objects.all()
 
     def get_queryset(self):
@@ -18,10 +38,16 @@ class ProjectIssuesView(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.R
     
     serializer_class = IssueSerializer
 
-class ProjectIssueCommentsView(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+class ProjectIssueCommentsView(ActionPermissions, viewsets.GenericViewSet,
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
+    permission_classes = (IsAuthenticated,)
+    permission_classes_by_action = {'list': [AllowAny]}
+
     queryset = Comment.objects.all()
     def get_queryset(self):
         pk = self.request.GET["issue"]
         issue = get_object_or_404(Issue, pk=pk)
         return super().get_queryset().filter(issue=issue)
+    
     serializer_class = CommentSerializer
